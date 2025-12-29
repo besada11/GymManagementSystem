e4a07ff5678f6e155587be7530c160894f0fcc72
Besada <Besada@DESKTOP-7OPJLV2>
2025-12-29 23:40:57 +0200

    Repository Pattertn

---
+++ GymManagementDAL/Repositories/Classes/MemberRepository.cs
@@ -0,0 +1,49 @@
+﻿using GymManagementDAL.Data.Context;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementDAL.Repositories.Classes
+{
+    internal class MemberRepository(GymDbContext _dbContext) : IMemberRepository
+
+    {
+        //Add
+        public int Add(Member member)
+        {
+            _dbContext.Members.Add(member);
+            return _dbContext.SaveChanges();
+        }
+
+        //Delete
+        public int Delete(Member member)
+        {
+            var existingMember = _dbContext.Members.Find(member.Id);
+            if (existingMember == null) return 0;
+
+            _dbContext.Members.Remove(existingMember);
+            return _dbContext.SaveChanges();
+        }
+
+        //GetAll
+        public IEnumerable<Member> GetAllMembers()
+        {
+            return _dbContext.Members.ToList();
+        }
+
+        //GetById
+        public Member? GetById(int id)
+        {
+            return _dbContext.Members.Find(id);
+        }
+
+        //Update
+        public int Update(Member member)
+        {
+            _dbContext.Members.Update(member);
+            return _dbContext.SaveChanges();  
+        }
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/IMemberRepository.cs
@@ -0,0 +1,25 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface IMemberRepository
+    {
+        //GetAll
+        IEnumerable<Member> GetAllMembers();
+
+        //GetById
+        Member? GetById(int id);    
+
+        //Add
+        int Add (Member member);
+
+        //Update
+        int Update(Member member);
+
+        //Delete
+        int Delete(Member member);
+    }
+}
