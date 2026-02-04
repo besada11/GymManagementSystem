1689fb3a568124f2a7b6cc7c7c194634f0e2a942
Besada <Besada@DESKTOP-7OPJLV2>
2026-02-05 01:51:18 +0200

    Member Controller - Details action

--- GymManagementPL/Controllers/MemberController.cs
+++ GymManagementPL/Controllers/MemberController.cs
@@ -5,10 +5,24 @@ namespace GymManagementPL.Controllers
 {
     public class MemberController(IMemberService _memberService) : Controller
     {
+        //Get All Members   
         public ActionResult Index()
         {
             var members = _memberService.GetAllMembers();   
             return View(members);
+        }
+
+        //Get Member Details by Id
+        public ActionResult MemberDetails(int id) 
+        {
+            if(id<= 0) 
+                return RedirectToAction(nameof(Index));
+
+            var member = _memberService.GetMemberDetails(id);
+            if(member == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(member);
         }
     }
 }
