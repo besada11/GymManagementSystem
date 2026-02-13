b307ba2809017be65c439f4cd7ab86b882a04682
Besada <Besada@DESKTOP-7OPJLV2>
2026-02-14 00:03:08 +0200

    Plan Controller -  Details

--- GymManagementPL/Controllers/PlanController.cs
+++ GymManagementPL/Controllers/PlanController.cs
@@ -5,10 +5,28 @@ namespace GymManagementPL.Controllers
 {
     public class PlanController(IPlanService _planService) : Controller
     {
+        //Get all plans
         public IActionResult Index()
         {
             var plans = _planService.GetAllPlan();
             return View(plans);
+        }
+
+        //Plan details
+        public IActionResult Details(int id) 
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid plan ID.";
+                return RedirectToAction(nameof(Index));
+            }
+            var plan = _planService.GetPlanById(id);
+            if (plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
         }
     }
 }
