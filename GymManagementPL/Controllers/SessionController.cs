b8cbf5eed734b0047c98c7a1752275b080bb69ee
Besada <Besada@DESKTOP-7OPJLV2>
2026-02-15 19:30:15 +0200

    Session Controller - Details Acion

--- GymManagementPL/Controllers/SessionController.cs
+++ GymManagementPL/Controllers/SessionController.cs
@@ -11,5 +11,22 @@ namespace GymManagementPL.Controllers
             var sessions = _sessionService.GetAllSessions();
             return View(sessions);
         }
+
+        //Details of a session
+        public IActionResult Details(int id)
+        {
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid session ID.";
+                return RedirectToAction("Index");
+            }
+            var session = _sessionService.GetSessionByID(id);
+            if(session == null)
+            {
+                TempData["ErrorMessage"] = "Session not found.";
+                return RedirectToAction("Index");
+            }
+            return View(session);
+        }
     }
 }
